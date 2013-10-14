032ba1c749f5f2a23630c2fc39ddfa7d03fbe25a
Shantanu Goel <shantanu@shantanugoel.com>
2013-10-14 19:20:37 +0530

    Initial version

---
+++ ExchangeBypassForXposed/src/main/java/com/shantz/exchangebypass/XposedModule.java
@@ -0,0 +1,78 @@
+package com.shantz.exchangebypass;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.setBooleanField;
+import static de.robv.android.xposed.XposedHelpers.setIntField;
+
+
+/**
+ * Created by shantanu on 10/13/13.
+ */
+public class XposedModule implements IXposedHookLoadPackage {
+    XC_MethodReplacement returnTrue = new XC_MethodReplacement() {
+        @Override
+        protected Object replaceHookedMethod(MethodHookParam param)
+                throws Throwable {
+            XposedBridge.log("returntrue hook run");
+            return true;
+        }
+    };
+
+    XC_MethodReplacement returnZero = new XC_MethodReplacement() {
+        @Override
+        protected Object replaceHookedMethod(MethodHookParam param)
+                throws Throwable {
+            XposedBridge.log("returnzero hook run");
+            return 0;
+        }
+    };
+
+    XC_MethodHook updatePolicy = new XC_MethodHook() {
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            XposedBridge.log("Normalize hook run");
+            setIntField(param.thisObject, "mPasswordMode", 0);
+            setBooleanField(param.thisObject, "mRequireRemoteWipe", false);
+            setBooleanField(param.thisObject, "mRequireEncryption", false);
+            setBooleanField(param.thisObject, "mRequireEncryptionExternal", false);
+            setBooleanField(param.thisObject, "mRequireManualSyncWhenRoaming", false);
+            setBooleanField(param.thisObject, "mDontAllowCamera", false);
+            setBooleanField(param.thisObject, "mDontAllowAttachments", false);
+            setBooleanField(param.thisObject, "mDontAllowHtml", false);
+        }
+        @Override
+        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+
+        }
+    };
+
+    public void handleLoadPackage(final LoadPackageParam lpparam)
+            throws Throwable {
+        if (lpparam.packageName.equals("com.google.android.email")) {
+            XposedBridge.log("Loaded app: " + lpparam.packageName);
+            hookEmail(lpparam);
+        } else if (lpparam.packageName.equals("com.google.android.exchange")) {
+            XposedBridge.log("Loaded app: " + lpparam.packageName);
+            hookExchange(lpparam);
+        }
+    }
+
+    private void hookExchange(LoadPackageParam lpparam) {
+        findAndHookMethod("com.android.exchange.adapter.ProvisionParser",
+                lpparam.classLoader, "hasSupportablePolicySet", returnTrue);
+    }
+
+    private void hookEmail(LoadPackageParam lpparam) {
+        findAndHookMethod("com.android.emailcommon.provider.Policy",
+                lpparam.classLoader, "normalize", updatePolicy);
+        findAndHookMethod("com.android.email.SecurityPolicy",
+                lpparam.classLoader, "getInactiveReasons", "com.android.emailcommon.provider.Policy",
+                returnZero);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,31 @@
+Exchange Security Bypass Xposed Mode for Nexus 4 (Android 4.3)
+
+This mod should be used along with Xposed ( http://forum.xda-developers.com/showthread.php?t=1574401 ).
+
+This mod will bypass the Email app from becoming device admin for your phone and prevent it from setting up any security restrictions on your device including pin/password/remote wipe, etc.
+
+Instructions for users
+- Remove your existing email account from phone and reboot
+- Install Xposed installer (from link above)
+- Install this mod (Download apk from here: )
+- Enable the mod in xposed (Preferences for other devices/versions coming Soon)
+- Reboot
+- Add your email account again. This time, it will ask you that the email
+  might require security restrictions, say yes to it but it will never actually set the restrictions.
+- Done. :)
+
+Instructions for developers
+- Clone this git repo
+- Import in Android Studio
+- Make changes
+- Build :)
+
+Credits:
+- rovo89 from xda (for creating xposed)
+- mpcjanssen from xda (for original mod)
+
+Please visit http://tech.shantanugoel.com/ for any queries and feedback
+
+Changelog:
+1.0 14-Oct-13
+Initial version. Working for Nexus 4 Android 4.3
